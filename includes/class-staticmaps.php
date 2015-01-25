aec116af931a21f0c450558ba40880bee0a12e5f
ninnypants <tyrel@ninnypants.com>
2015-01-24 18:04:05 -0700

    Setup StaticMaps::prep_param() and StaticMaps::prep_zoom()

--- includes/class-staticmaps.php
+++ includes/class-staticmaps.php
@@ -4,6 +4,18 @@
  */
 class StaticMaps {
 	protected $base_url = 'https://maps.googleapis.com/maps/api/staticmap';
+	protected $defaults = array(
+		'center' => 'Provo, UT',
+		'zoom' => 12,
+		'width' => 500,
+		'height' => 400,
+		// 'size' => '500x400',
+		'scale' => 1,
+		'format' => 'PNG',
+		'maptype' => 'roadmap',
+		// 'language' => '',
+		// 'region' => '',
+	);
 
 	public function __construct() {
 		add_shortcode( 'map', array( $this, 'shortcode_map' ) );
@@ -12,18 +24,7 @@ class StaticMaps {
 	}
 
 	public function shortcode_map( $atts, $content, $tag ) {
-		$args = wp_parse_args( $atts, array(
-			'center' => 'Provo, UT',
-			'zoom' => 12,
-			'width' => 500,
-			'height' => 400,
-			// 'size' => '500x400',
-			'scale' => 1,
-			'format' => 'PNG',
-			'maptype' => 'roadmap',
-			// 'language' => '',
-			// 'region' => '',
-		) );
+		$args = wp_parse_args( $atts, $this->defaults );
 
 		$output_url = $this->base_url;
 
@@ -37,13 +38,32 @@ class StaticMaps {
 
 	}
 
+	/**
+	 * Do basic prep work for all params
+	 *
+	 * @param  mixed $value Param to be preped for use
+	 * @return mixed Processed param ready to be used
+	 */
 	public function prep_param( $value ) {
 		return trim( $value );
 	}
 
+	/**
+	 * Make sure the zoom param is an integer, and within the range
+	 * of 0 to 22
+	 *
+	 * @param  int $value Zoom level to be used for the map
+	 * @return int A valid zoom level to be used
+	 */
 	public function prep_zoom( $value ) {
-		$center = $this->prep_param( $value );
-		$center = (int) $center;
+		$zoom = $this->prep_param( $value );
+
+		if ( ! is_numeric( $zoom ) ) {
+			return $this->defaults['zoom'];
+		}
+
+		// make sure $zoom is actually an int
+		$zoom = (int) $zoom;
 
 		if ( $zoom < 0 ) {
 			$zoom = 0;
--- tests/tests-staticmaps.php
+++ tests/tests-staticmaps.php
@@ -23,4 +23,47 @@ class StaticMaps_Tests extends PHPUnit_Framework_TestCase {
 	public function tearDown() {
 		\WP_Mock::tearDown();
 	}
+
+	public function test_prep_param() {
+		$this->assertEmpty( self::$instance->prep_param( ' ' ) );
+		$this->assertEquals( 'param', self::$instance->prep_param( 'param ' ) );
+		$this->assertEquals( 'param', self::$instance->prep_param( ' param' ) );
+		$this->assertEquals( 'param', self::$instance->prep_param( ' param ' ) );
+	}
+
+	public function test_prep_zoom() {
+		// make sure everything stays within bounds
+		$this->assertEquals( 0, self::$instance->prep_zoom( -1 ) );
+		$this->assertEquals( 22, self::$instance->prep_zoom( 300 ) );
+
+		// test proper value
+		$this->assertEquals( 10, self::$instance->prep_zoom( 10 ) );
+
+		// make sure we get a default if we don't pass an int
+		$this->assertEquals( 12, self::$instance->prep_zoom( 'zoom' ) );
+	}
+
+	public function test_prep_format() {
+		$this->markTestIncomplete();
+	}
+
+	public function test_prep_scale() {
+		$this->markTestIncomplete();
+	}
+
+	public function test_prep_maptype() {
+		$this->markTestIncomplete();
+	}
+
+	public function test_shortcode_map() {
+		$this->markTestIncomplete();
+	}
+
+	public function test_shortcode_marker() {
+		$this->markTestIncomplete();
+	}
+
+	public function test_shortcode_location() {
+		$this->markTestIncomplete();
+	}
 }
